566b2014c2ad00564c81e0f5b559544ad3165183
Simeon Tsekov <simeon.1.tsekov@gmail.com>
2020-06-13 17:30:31 +0300

    SD: Changed root directory name.

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,22 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
 function style(){
-    return gulp.src('./Project/scss/**/*.scss')
+    return gulp.src('./docs/scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./Project/css'))
+        .pipe(gulp.dest('./docs/css'))
         .pipe(browserSync.stream());
 }
 
 function watch(){
     browserSync.init({
         server: {
-            baseDir: './Project',
+            baseDir: './docs',
             index: './index.html'
         }
     });
-    gulp.watch('./Project/scss/**/*.scss', style);
-    gulp.watch('./Project/**/*.html').on('change', browserSync.reload);
-    gulp.watch('./Project/js/**/*.js').on('change', browserSync.reload);
+    gulp.watch('./docs/scss/**/*.scss', style);
+    gulp.watch('./docs/**/*.html').on('change', browserSync.reload);
+    gulp.watch('./docs/js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
